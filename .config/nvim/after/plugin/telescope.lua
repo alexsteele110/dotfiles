5cad9845492f50a16dbf3b9e82878ae08983688f
Alex Steele <alexsteele110@gmail.com>
2024-11-19 14:18:53 -0500

    small nvm config updates

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -17,4 +17,4 @@ require('telescope').setup{
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.grep_string, {})
-vim.keymap.set('n', '<leader>pg', builtin.live_grep, {}) 
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
--- .config/nvim/lua/asteele/packer.lua
+++ .config/nvim/lua/asteele/packer.lua
@@ -13,7 +13,7 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-	use { "catppuccin/nvim", as = "catppuccin" } 
+	use { "catppuccin/nvim", as = "catppuccin" }
 	use { 'rose-pine/neovim', as = 'rose-pine' }
 
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
